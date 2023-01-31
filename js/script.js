5f7e3764e00d1ed121d119f7fe7407b11451f060
aimee rivers <aimee@aimeerivers.com>
2023-01-31 16:56:25 +0100

    Adds abililty to change the label text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const audio = new Audio("https://www.aimeerivers.com/push-the-button/audio/button.mp3");
 const button = document.getElementById("button");
+const label = document.getElementById("label");
 
 button.addEventListener("click", () => {
   audio.play();
 });
+
+label.textContent="Push me now!";
