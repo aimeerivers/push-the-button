c0426163e31f56112b81a27b574e651056752ae8
aimee rivers <aimee@aimeerivers.com>
2023-01-31 16:51:11 +0100

    Relative links are hard

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const audio = new Audio("../audio/button.mp3");
+const audio = new Audio("https://www.aimeerivers.com/push-the-button/audio/button.mp3");
 const button = document.getElementById("button");
 
 button.addEventListener("click", () => {
